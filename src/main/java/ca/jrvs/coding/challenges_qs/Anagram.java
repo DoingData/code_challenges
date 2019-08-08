231e4ffe0cfc5667c3aa7597a69ef24b20979753
usmanasifjrvs <usman.asif.jrvs.ca@gmail.com>
2019-08-08 02:14:13 -0400

    update

--- src/main/java/ca/jrvs/coding/challenges_qs/Anagram.java
+++ src/main/java/ca/jrvs/coding/challenges_qs/Anagram.java
@@ -1,27 +1,19 @@
 package ca.jrvs.coding.challenges_qs;
-
 import java.util.Arrays;
-
 /**
  * Given two strings s and t , write a function to determine if t is an anagram of s.
  */
 public class Anagram {
-
     public boolean isAnagram(String s, String t) {
         if (s.length() == t.length()) {
             char[] str1 = s.toCharArray();
             char[] str2 = t.toCharArray();
-
             Arrays.sort(str1);
             Arrays.sort(str2);
-
             if (Arrays.equals(str1, str2)) {
                 return true;
             }
         }
-
         return false;
-
     }
-
 }
